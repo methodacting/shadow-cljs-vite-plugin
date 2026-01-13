bf1d12ccdb3f963ba31e8aff1a1e1f3517f55328
c4605 <bolasblack@gmail.com>
2026-01-13 10:54:50 +0100

    Revert "feat: implement recursive process tree killing logic refined from node-tree-kill"
    This reverts commit 7e2fa4ad8699bb36d3fdca7e2291bd0e02795e75.

--- src/utils/killProcess.ts
+++ src/utils/killProcess.ts
@@ -1,168 +1,34 @@
-/**
- * code from https://github.com/pkrumins/node-tree-kill/blob/cb478381547107f5c53362668533f634beff7e6e/index.js
- */
-
-import { spawn, exec, type ChildProcess } from "child_process";
-
-export function treeKill(
-  pid: number | `${number}`,
-  signal?: NodeJS.Signals | number,
-  callback?: (err?: Error) => void
-): void {
-  if (typeof signal === "function") {
-    callback = signal;
-    signal = undefined;
-  }
-
-  const pidInt = typeof pid === "string" ? parseInt(pid, 10) : pid;
-  if (Number.isNaN(pidInt)) {
-    if (callback) {
-      return callback(new Error("pid must be a number"));
-    } else {
-      throw new Error("pid must be a number");
-    }
-  }
-
-  const tree: Record<number, number[]> = {};
-  const pidsToProcess: Record<number, number> = {};
-  tree[pidInt] = [];
-  pidsToProcess[pidInt] = 1;
-
-  switch (process.platform) {
-    case "win32":
-      exec(`taskkill /pid ${pidInt} /T /F`, (err) => {
-        if (callback) callback(err ?? undefined);
-      });
-      break;
-    case "darwin":
-      buildProcessTree(
-        pidInt,
-        tree,
-        pidsToProcess,
-        (parentPid) => spawn("pgrep", ["-P", String(parentPid)]),
-        () => {
-          killAll(tree, signal, callback);
-        }
-      );
-      break;
-    default: // Linux
-      buildProcessTree(
-        pidInt,
-        tree,
-        pidsToProcess,
-        (parentPid) =>
-          spawn("ps", [
-            "-o",
-            "pid",
-            "--no-headers",
-            "--ppid",
-            String(parentPid),
-          ]),
-        () => {
-          killAll(tree, signal, callback);
-        }
-      );
-      break;
-  }
-}
-
-function killAll(
-  tree: Record<number, number[]>,
-  signal?: NodeJS.Signals | number,
-  callback?: (err?: Error) => void
-) {
-  const killed: Record<number, number> = {};
-  try {
-    Object.keys(tree).forEach((pidStr) => {
-      const pid = parseInt(pidStr, 10);
-      tree[pid].forEach((pidpid) => {
-        if (!killed[pidpid]) {
-          killPid(pidpid, signal);
-          killed[pidpid] = 1;
-        }
-      });
-      if (!killed[pid]) {
-        killPid(pid, signal);
-        killed[pid] = 1;
-      }
-    });
-  } catch (err: any) {
-    if (callback) {
-      return callback(err);
-    } else {
-      throw err;
-    }
-  }
-  if (callback) {
-    return callback();
-  }
-}
-
-function killPid(pid: number, signal?: NodeJS.Signals | number) {
-  try {
-    process.kill(pid, signal);
-  } catch (err: any) {
-    if (err.code !== "ESRCH") throw err;
-  }
-}
-
-function buildProcessTree(
-  parentPid: number,
-  tree: Record<number, number[]>,
-  pidsToProcess: Record<number, number>,
-  spawnChildProcessesList: (pid: number) => ChildProcess,
-  cb: () => void
-) {
-  const ps = spawnChildProcessesList(parentPid);
-  let allData = "";
-  ps.stdout?.on("data", (data) => {
-    allData += data.toString("ascii");
-  });
-
-  const onClose = (code: number | null) => {
-    delete pidsToProcess[parentPid];
-
-    if (code !== 0) {
-      // no more parent processes
-      if (Object.keys(pidsToProcess).length === 0) {
-        cb();
-      }
-      return;
-    }
-
-    const matches = allData.match(/\d+/g);
-    if (matches) {
-      matches.forEach((pidStr) => {
-        const pid = parseInt(pidStr, 10);
-        tree[parentPid].push(pid);
-        tree[pid] = [];
-        pidsToProcess[pid] = 1;
-        buildProcessTree(pid, tree, pidsToProcess, spawnChildProcessesList, cb);
-      });
-    } else if (Object.keys(pidsToProcess).length === 0) {
-      cb();
-    }
-  };
-
-  ps.on("close", onClose);
-}
-
-export async function treeKillAsync(
-  pid: number | `${number}`,
-  signal?: NodeJS.Signals | number
-): Promise<void> {
-  return new Promise((resolve, reject) => {
-    treeKill(pid, signal, (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
-}
+import type { ChildProcess } from "child_process";
+import os from "os";
+import { spawnAsync } from "./spawnAsync";
 
 export async function killProcess(proc: ChildProcess) {
-  if (proc.pid) {
-    await treeKillAsync(proc.pid, "SIGKILL");
+  if (os.platform() === "win32") {
+    await spawnAsync("taskkill", ["/PID", String(proc.pid), "/T", "/F"], {
+      stdio: "ignore",
+    });
+  } else if (proc.pid == null) {
+    try {
+      proc.kill("SIGKILL");
+    } catch (e: any) {
+      if (e.code !== "ESRCH") throw e;
+    }
   } else {
-    proc.kill("SIGKILL");
+    const pid = proc.pid;
+    const doKill = (signal: number | NodeJS.Signals): boolean => {
+      try {
+        process.kill(-pid, signal);
+        return false;
+      } catch (e: any) {
+        if (e.code === "ESRCH") return true;
+        throw e;
+      }
+    };
+
+    doKill("SIGINT");
+    while (/* check if process is still alive */ !doKill(0)) {
+      await new Promise((r) => setTimeout(r, 100));
+      if (doKill("SIGKILL")) break;
+    }
   }
 }
